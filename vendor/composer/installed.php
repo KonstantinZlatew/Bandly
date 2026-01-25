175e7fed400ab1c7c7558c072e641d5c0c3bd9f7
Konstantin Zlatev <kzlatev07@gmail.com>
2026-01-25 17:21:00 +0200

    Intall stripe php package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4521fcec879c1629fa590e4dc4cdd93fe2eb665d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4521fcec879c1629fa590e4dc4cdd93fe2eb665d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v19.2.0',
+            'version' => '19.2.0.0',
+            'reference' => 'e444fbc524cd3d6583dde2c5b375d26174c54d96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
